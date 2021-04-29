082f272c6f1f43757f7b2356197d602be0785a85
Alex <alexbachurin91@gmail.com>
2021-04-29 04:30:02 +0300

    standart non-dynamic method for navbar

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.nav__menu'),
           hamburger = document.querySelector('.hamburger')
 
-    //toggle menu
+    //toggle menu and hamburger
     hamburger.addEventListener('click', () => {
         menu.classList.toggle('show-nav');
         hamburger.classList.toggle('is-active')
