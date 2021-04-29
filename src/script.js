17695ab7e1d20d0e22a7e68d80987dcf463e75d4
Alex <alexbachurin91@gmail.com>
2021-04-29 04:43:01 +0300

    working dynamic navbar

--- src/script.js
+++ src/script.js
@@ -1,11 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const menu = document.querySelector('.nav__menu'),
-          hamburger = document.querySelector('.hamburger')
+          hamburger = document.querySelector('.hamburger'),
+          menuBlock = document.querySelector('.nav__menu-block')
 
     //toggle menu and hamburger
     hamburger.addEventListener('click', () => {
-        menu.classList.toggle('show-nav');
+        //get links block height
+        const linksHeight = menuBlock.getBoundingClientRect().height;
+        console.log(linksHeight)
+        const menuHeight = menu.getBoundingClientRect().height;
+        console.log(menuHeight)
+        if (menuHeight === 0) {
+            menu.style.height = `${linksHeight}px`;
+            console.log(menu.style.height)
+        } else {
+            menu.style.height = `0px`;
+        }
         hamburger.classList.toggle('is-active')
     })
 })
